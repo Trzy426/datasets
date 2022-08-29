498132cd09456ec1fd9bc8d748ca4c96c404d0d4
Trzy426 <charleswrogersiii@gmail.com>
2022-08-28 19:16:43 -0500

    used pandas to read through csv and put into table

--- python_work/main.py
+++ python_work/main.py
@@ -1,6 +1,12 @@
-
-
+import numpy as np
+import pandas as pd
+import googletrans
 
 def main():
-    """ Boiler, script """
-    print("WAI")
+    # reads in 6454 points in 5 columns
+    data = pd.read_csv("/home/trzy/dev/datasets/python_work/amazon.csv",encoding="ISO-8859-1", thousands = '.')
+    #strip all non fires from the table. Turn them to NaN then strip
+    data = data.replace(0,np.nan)
+    subset = data.dropna(subset=['number'])
+    print(subset.describe(include='all'))
+main()
