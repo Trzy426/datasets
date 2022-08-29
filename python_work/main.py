70b4c81a118e04a6042e04e9c84b418275be994f
Trzy426 <charleswrogersiii@gmail.com>
2022-08-29 02:39:56 -0500

    removed non fires

--- python_work/main.py
+++ python_work/main.py
@@ -5,8 +5,10 @@ import googletrans
 def main():
     # reads in 6454 points in 5 columns
     data = pd.read_csv("/home/trzy/dev/datasets/python_work/amazon.csv",encoding="ISO-8859-1", thousands = '.')
-    #strip all non fires from the table. Turn them to NaN then strip
-    data = data.replace(0,np.nan)
+    #replace non fires with nan
+    data = data.replace(0, np.nan)
     subset = data.dropna(subset=['number'])
+    #see what table looks like
     print(subset.describe(include='all'))
+    
 main()
